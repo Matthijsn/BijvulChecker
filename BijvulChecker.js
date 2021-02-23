fd2897cfebad6d4652f244eb94e37ce723a5624c
Matthijs Noordhof <matthijsnoordhof@hotmail.com>
2021-02-24 00:29:20 +0100

    added variables

--- BijvulChecker.js
+++ BijvulChecker.js
@@ -1,9 +1,19 @@
+//Variables
+var interval = 30;
+var Empty = 201;
+var readtimeout = 1;
+
+//Init aduio and notification
 Notification.requestPermission();
 var audio = new Audio("https://cdn.discordapp.com/attachments/651377520406429700/812359200037404752/juntos-607.mp3");
-$('.grid-my-container').prepend('<button id="Start" onclick=Start()>Start</button>');
-$('.grid-my-container').prepend('<button id="Stop" onclick=Stop()>Stop</button>');
-$('.grid-my-container').prepend('<button onclick=audio.play()>Test Geluid</button>');
+
+//Adding visual elements
+// $('.grid-my-container').prepend('<button id="Start" onclick=Start()>Start</button>');
+// $('.grid-my-container').prepend('<button id="Stop" onclick=Stop()>Stop</button>');
+$('.grid-my-container').prepend('<button onclick=Message()>Test Melding</button>');
 $('#Stop').attr('disabled', 'true');
+
+//Gets the mbs left
 GetMB = function(){
 var NogOver;
 if($('.text-confirm').length > 0){
@@ -16,22 +26,30 @@ NogOver = parseInt($('.text-warning')[0].innerHTML);
 // console.log(parseInt($('.text-error')[0].innerHTML())); 
 NogOver = parseInt($('.text-error')[0].innerHTML);
 }
-if(NogOver <= 201 && $('.text-confirm').length == 0){
+if(NogOver <= Empty && $('.text-confirm').length == 0){
+//Plays the audio, change the ui an sends a notification when the bundle is almost empty
+Message();
+}
+console.log(NogOver);
+console.log(Empty);
+}
+
+//Starts the process
+Start = function(){
+$('#Start').attr('disabled', 'true');
+$('#Stop').attr('disabled', 'false');
+setInterval(function(){
+$('#RefreshUsageButton').click();
+setTimeout(() => {  GetMB() }, readtimeout*1000);
+}, interval*1000);
+}
+Message = function(){
 audio.play();
 $('.bundle-usage-table').css('background', 'red');
 new Notification('De Bundel is bijna op!', { Title: 'Let op!', Icon: 'https://www.t-mobile.nl/assets/static/t-mobile-logo.svg?v=2018' }).addEventListener('click', function(){
     window.open('https://www.t-mobile.nl/my/aanvullers');
 });
 }
-console.log(NogOver);
-}
-Start = function(){
-$('#Start').attr('disabled', 'true');
-$('#Stop').attr('disabled', 'false');
-setInterval(function(){
-$('#RefreshUsageButton').click();
-setTimeout(() => {  GetMB() }, 1000);
-}, 30000);
-}
+Start();
 
 // ©Matthijs Noordhof 2020
