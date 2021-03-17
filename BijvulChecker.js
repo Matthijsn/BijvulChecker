b8fa9e59b2a172238647dc082b3ba65c022646db
Matthijsn <44608102+Matthijsn@users.noreply.github.com>
2021-03-17 11:24:14 +0100

    --update--
    Vanaf nu zijn 2 gigabyte-aanvullters de standaard. Aanvullen is dus nu mogelijk vanaf 401 mb.

--- BijvulChecker.js
+++ BijvulChecker.js
@@ -1,11 +1,12 @@
 //Variables
 var interval = 30;
-var Empty = 201;
+var Empty = 401;
 var readtimeout = 1;
 
 //Init aduio and notification
 Notification.requestPermission();
 var audio = new Audio("https://cdn.discordapp.com/attachments/651377520406429700/812359200037404752/juntos-607.mp3");
+var error = new Audio("https://cdn.discordapp.com/attachments/651377520406429700/815677083857977384/chimes-power-down-190.mp3");
 
 //Adding visual elements
 // $('.grid-my-container').prepend('<button id="Start" onclick=Start()>Start</button>');
@@ -44,12 +45,18 @@ setTimeout(() => {  GetMB() }, readtimeout*1000);
 }, interval*1000);
 }
 Message = function(){
-//audio.play();
+audio.play();
 //$('.bundle-usage-table').css('background', 'red');
-new Notification('De Bundel is bijna op!', { Title: 'Let op!', icon: 'https://content.presspage.com/clients/o_206.png' }).addEventListener('click', function(){
+new Notification('De Bundel is bijna op!', { body: 'Klik hier om bij te vullen', icon: 'https://content.presspage.com/clients/o_206.png' }).addEventListener('click', function(){
     window.open('https://www.t-mobile.nl/my/aanvullers');
 });
 }
 Start();
+window.onbeforeunload = function() {
+// new Notification('Oeps!', { body: 'De pagina is opnieuw geladen, plak de code opnieuw in de console', icon: 'https://content.presspage.com/clients/o_206.png' }).addEventListener('click', function(){
+//     window.open('');
+// });
+// error.play();
+}
 
 // ©Matthijs Noordhof 2020
