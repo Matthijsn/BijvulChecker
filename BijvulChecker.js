87c611471367f6f7ac8dcd61399642c589ead574
Matthijs Noordhof <matthijsnoordhof@hotmail.com>
2021-04-12 21:11:29 +0200

    update
    - Now reloads the page after a given amount of calls, you can change this in the variables.
    - Only calls inside a given set of hours, you can also change this in the variables.

--- BijvulChecker.js
+++ BijvulChecker.js
@@ -1,11 +1,19 @@
 //Variables
-var interval = 30;
+var interval = 40;
 var Empty = 201;
 var readtimeout = 1;
+var reloadAfter = 18000/interval;
+var count = 0;
 
-//Init aduio and notification
+// For getting the time
+var Begin = 09;
+var Stop = 01;
+var currentHour;
+
+//Init audio and notification
 Notification.requestPermission();
 var audio = new Audio("https://cdn.discordapp.com/attachments/651377520406429700/812359200037404752/juntos-607.mp3");
+var error = new Audio("https://cdn.discordapp.com/attachments/651377520406429700/815677083857977384/chimes-power-down-190.mp3");
 
 //Adding visual elements
 // $('.grid-my-container').prepend('<button id="Start" onclick=Start()>Start</button>');
@@ -13,8 +21,21 @@ var audio = new Audio("https://cdn.discordapp.com/attachments/651377520406429700
 $('.grid-my-container').prepend('<button onclick=Message()>Test Melding</button>');
 $('#Stop').attr('disabled', 'true');
 
+
 //Gets the mbs left
 GetMB = function(){
+// Doesn't get the mbs outside the given times
+
+//Use when end time is a bigger digit than the begin time
+//if(currentHour < Begin || currentHour >= Stop){
+
+//Checks if the time is inside the given hours
+if(currentHour < Begin && currentHours > Stop){
+console.log("Gestopt");
+}else if(count == reloadAfter){
+// Reloads the page after a given amount of calls
+location.reload();
+}else{
 var NogOver;
 if($('.text-confirm').length > 0){
 // console.log(parseInt($('.text-confirm')[0].innerHTML));
@@ -32,24 +53,42 @@ Message();
 }
 console.log(NogOver);
 console.log(Empty);
+count++;
 }
+}
+
 
 //Starts the process
 Start = function(){
 $('#Start').attr('disabled', 'true');
 $('#Stop').attr('disabled', 'false');
 setInterval(function(){
+//Gets the current hour
+currentHour = new Date().getHours();
+
+//Use when end time is a bigger digit than the begin time
+//if(currentHour < Begin || currentHour >= Stop){
+
+//Checks if the time is inside the given hours
+if(currentHour < Begin && currentHours > Stop){
+}else{
 $('#RefreshUsageButton').click();
+}
 setTimeout(() => {  GetMB() }, readtimeout*1000);
 }, interval*1000);
 }
 Message = function(){
-//audio.play();
+audio.play();
 //$('.bundle-usage-table').css('background', 'red');
 new Notification('De Bundel is bijna op!', { Title: 'Let op!', icon: 'https://content.presspage.com/clients/o_206.png' }).addEventListener('click', function(){
     window.open('https://www.t-mobile.nl/my/aanvullers');
 });
 }
 Start();
+
+$(window).bind('beforeunload',function(){
+// error.play();
+
+});
 
 // ©Matthijs Noordhof 2020
